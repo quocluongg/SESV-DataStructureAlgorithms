b56d8c2f16bd5db30c5fb7717f7393c0d83af766
Khoi Quoc <likuroquoc@gmail.com>
2024-11-16 11:36:05 +0700

    add lecture 7 8

--- lecture-06_map-hashmap/phonebook.js
+++ lecture-06_map-hashmap/phonebook.js
@@ -1,10 +1,8 @@
 const phonebook = new Map();
-
 for (let i = 1; i <= 100000; i++) {
   const name = `Anh ${i}`;
   const phoneNumber = `0797570${i}`;
   phonebook.set(name, phoneNumber);
 }
-
 console.log(phonebook.get("Anh 2"));
 console.log(phonebook.get("Anh 988"));
